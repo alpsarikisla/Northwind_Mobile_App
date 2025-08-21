3bb3b01e520ce82d69450c79823ccdd9fbff0263
Alp Sarıkışla <alpsarikisla@hotmail.com>
2025-08-21 13:13:30 +0300

    initial commit

---
+++ app/index.jsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+
+const Home = () => {
+  return (
+    <View>
+      <Text>Nabünüz</Text>
+      <Text>Armağan naber???</Text>
+      <Text>Kalk git lan gokmuş</Text>
+    </View>
+  )
+}
+
+export default Home
+
+const styles = StyleSheet.create({})
